bdcd8aeb302af625d87aef20edc2832fac4b70ef
Meenatchi D <122455213+Meenatchid@users.noreply.github.com>
2023-01-13 20:25:27 +0530

    Update streamlit_app.py
    Normalize JSON format of response data and display it in table format

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit
-import pandas
 streamlit.title("My Mom's New Healthy Diner")
 streamlit.header("Breakfast Favorites")
 streamlit.text("🥣Omega 3 & Blueberry Oatmeal")
@@ -7,6 +6,8 @@ streamlit.text("🥗Kale, Spinach and Rocket Smoothie")
 streamlit.text("🐔Hard Boiled Free-Range Egg")
 streamlit.text("🥑🍞Avocado Toast")
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -20,5 +21,9 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
+
+# normalize json format of the respone
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display normalized data in a table format
+streamlit.dataframe(fruityvice_normalized)
